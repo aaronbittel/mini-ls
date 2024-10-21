dfee0485c64c3c3e39fd76c89070ff793b5e4bd9
Aaron Bittel <aaron@bittels.de>
2024-10-21 15:33:23 +0200

    feat: -lr, -a kinda works

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+use chrono::{DateTime, Utc};
 use std::error::Error;
 use std::fs;
-use std::io::{self, Write};
+use std::os::unix::fs::MetadataExt;
 use std::path;
+use std::time::SystemTime;
+use users::{get_group_by_gid, get_user_by_uid};
 
 #[allow(non_snake_case)]
 #[derive(Debug, PartialEq)]
@@ -18,38 +21,174 @@ pub struct Config {
     d: bool,
 }
 
+#[derive(Clone)]
+pub struct FileInfo {
+    permissions: String,
+    link_count: u64,
+    owner: String,
+    group: String,
+    size: u64,
+    modified: String,
+    name: String,
+    file_type: FileType,
+}
+
+impl FileInfo {
+    fn new(
+        permissions: String,
+        link_count: u64,
+        owner: String,
+        group: String,
+        size: u64,
+        modified: String,
+        name: String,
+        file_type: FileType,
+    ) -> Self {
+        Self {
+            permissions,
+            link_count,
+            owner,
+            group,
+            size,
+            modified,
+            name,
+            file_type,
+        }
+    }
+
+    fn long(&self) -> String {
+        const COLOR_BLUE: &str = "\x1b[1;38;5;12m";
+        const ANSI_RESET: &str = "\x1b[0m";
+        const COLOR_CYAN: &str = "\x1b[1;38;5;14m";
+
+        let color = if self.file_type == FileType::Dir {
+            COLOR_BLUE
+        } else {
+            ""
+        };
+
+        let name = match &self.file_type {
+            FileType::Symlink(target) => match target.to_str() {
+                Some(target) => format!("{COLOR_CYAN}{}{ANSI_RESET} -> {target}", self.name),
+                None => "".to_string(),
+            },
+            _ => self.name.clone(),
+        };
+
+        format!(
+            "{} {:<2} {:>5} {:>5} {:>4} {} {color}{name}{ANSI_RESET}",
+            self.permissions, self.link_count, self.owner, self.group, self.size, self.modified,
+        )
+    }
+
+    fn short(&self) -> String {
+        const COLOR_BLUE: &str = "\x1b[1;38;5;12m";
+        const ANSI_RESET: &str = "\x1b[0m";
+        const COLOR_CYAN: &str = "\x1b[1;38;5;14m";
+
+        let color = if self.file_type == FileType::Dir {
+            COLOR_BLUE
+        } else if let FileType::Symlink(_) = self.file_type {
+            COLOR_CYAN
+        } else {
+            ""
+        };
+
+        format!("{color}{}{ANSI_RESET}", self.name)
+    }
+
+    fn file_name(&self) -> String {
+        self.name.clone()
+    }
+}
+
+#[derive(Clone, PartialEq)]
+enum FileType {
+    File,
+    Dir,
+    Symlink(path::PathBuf),
+}
+
+struct LSDisplay {
+    config: Config,
+    entries: Vec<FileInfo>,
+}
+
+impl LSDisplay {
+    fn new(config: Config, mut entries: Vec<FileInfo>) -> Self {
+        entries.sort_by_key(|e| e.file_name().to_lowercase());
+        if config.r {
+            entries.reverse();
+        }
+
+        Self { config, entries }
+    }
+
+    fn display(&self) {
+        let entries = if self.config.a {
+            &self.entries
+        } else {
+            &self
+                .entries
+                .iter()
+                .filter(|entry| !entry.name.starts_with('.'))
+                .cloned()
+                .collect::<Vec<_>>()
+        };
+
+        for entry in entries {
+            if self.config.l {
+                println!("{}", entry.long());
+            } else {
+                print!("{}  ", entry.short());
+            }
+        }
+    }
+}
+
+// TODO: Handle "." and ".."
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut entries: Vec<fs::DirEntry> = vec![];
+    let mut entries: Vec<FileInfo> = vec![];
     for entry in fs::read_dir(&config.path)? {
         let entry = entry?;
-        if let Some(e) = entry.file_name().to_str() {
-            if !e.starts_with(".") {
-                entries.push(entry);
-            }
-        }
+        let fileinfo = create_fileinfo(entry)?;
+        entries.push(fileinfo);
     }
 
-    let sep = if config.l { "\n" } else { "  " };
-
-    entries.sort_by_key(|e| e.file_name());
-
-    const blue: &str = "\x1b[1;38;5;12m";
-    const reset: &str = "\x1b[0m";
-
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
-    for entry in entries {
-        if let Some(e) = entry.file_name().to_str() {
-            if entry.file_type()?.is_dir() {
-                write!(handle, "{blue}{e}{reset}{sep}");
-            } else {
-                write!(handle, "{e}{sep}");
-            }
-        }
-    }
-    writeln!(handle);
+    let printer = LSDisplay::new(config, entries);
+    printer.display();
 
     Ok(())
+}
+
+fn format_system_time(time: SystemTime) -> String {
+    // Convert SystemTime to DateTime<Utc>
+    let datetime: DateTime<Utc> = time.into(); // SystemTime::into converts to DateTime<Utc>
+
+    // Format the date and time
+    datetime.format("%b %d %H:%M").to_string() // Format as "Oct 18 14:37"
+}
+
+fn get_owner_and_group_names(owner_id: u32, group_id: u32) -> Result<(String, String), String> {
+    // Get owner name
+    let owner_name = match get_user_by_uid(owner_id) {
+        Some(user) => match user.name().to_str() {
+            Some(u) => u.to_string(),
+            None => "Unknown user".to_string(),
+        },
+        None => "Unknown user".to_string(),
+    };
+
+    // Get group name
+    let group_name = match get_group_by_gid(group_id) {
+        Some(group) => match group.name().to_str() {
+            Some(g) => g.to_string(),
+            None => "Unknown group".to_string(),
+        },
+        None => "Unknown group".to_string(),
+    };
+
+    Ok((owner_name, group_name))
 }
 
 impl Config {
@@ -74,19 +213,8 @@ impl Config {
 
         while let Some(arg) = args.next() {
             if arg.starts_with("-") {
-                for a in arg.chars().skip(1) {
-                    match a {
-                        'l' => config.with_l(),
-                        'a' => config.with_a(),
-                        'F' => config.with_F(),
-                        'h' => config.with_h(),
-                        'S' => config.with_S(),
-                        'R' => config.with_R(),
-                        'r' => config.with_r(),
-                        't' => config.with_t(),
-                        'd' => config.with_d(),
-                        _ => {}
-                    }
+                for flag in arg.chars().skip(1) {
+                    config.set_flag(flag);
                 }
             } else {
                 config.set_path(arg);
@@ -100,49 +228,121 @@ impl Config {
         self.path = path::PathBuf::from(path);
     }
 
-    fn with_l(&mut self) {
-        self.l = true;
+    fn set_flag(&mut self, flag: char) {
+        match flag {
+            'l' => self.l = true,
+            'a' => self.a = true,
+            'F' => self.F = true,
+            'h' => self.h = true,
+            'S' => self.S = true,
+            'R' => self.R = true,
+            'r' => self.r = true,
+            't' => self.t = true,
+            'd' => self.d = true,
+            _ => {}
+        }
+    }
+}
+
+// TODO: Handle Error
+fn get_perm_str(perm: u32) -> String {
+    match perm {
+        0 => "---".to_string(),
+        1 => "--x".to_string(),
+        2 => "-w-".to_string(),
+        4 => "r--".to_string(),
+        5 => "r-x".to_string(),
+        6 => "rw-".to_string(),
+        7 => "rwx".to_string(),
+        _ => "".to_string(),
+    }
+}
+
+fn get_mode(file_type: &FileType, mode: u32) -> String {
+    let owner_perm = (mode / 64) % 8;
+    let group_perm = (mode / 8) % 8;
+    let other_perm = mode % 8;
+
+    let mut mode = String::with_capacity(10);
+    match file_type {
+        FileType::Dir => mode.push_str("d"),
+        FileType::File => mode.push_str("-"),
+        FileType::Symlink(_) => mode.push_str("l"),
     }
 
-    fn with_a(&mut self) {
-        self.a = true;
+    mode.push_str(&get_perm_str(owner_perm));
+    mode.push_str(&get_perm_str(group_perm));
+    mode.push_str(&get_perm_str(other_perm));
+
+    mode
+}
+
+fn create_fileinfo(entry: fs::DirEntry) -> Result<FileInfo, Box<dyn Error>> {
+    let path = entry.path();
+    let metadata = fs::metadata(&path)?;
+    let mode = metadata.mode();
+    let file = entry.file_type()?;
+    let filetype: FileType;
+    if file.is_dir() {
+        filetype = FileType::Dir;
+    } else if file.is_file() {
+        filetype = FileType::File;
+    } else {
+        match fs::read_link(path) {
+            Ok(target) => {
+                filetype = FileType::Symlink(target);
+            }
+            Err(_) => return Err("could not get symlink target".into()),
+        }
+    }
+    let perm = get_mode(&filetype, mode);
+    let nlinks = metadata.nlink();
+    let (owner, group) = get_owner_and_group_names(metadata.uid(), metadata.gid())?;
+    let size = metadata.len();
+    let modified_time = metadata.modified()?;
+    let modified_time = format_system_time(modified_time);
+
+    if let Some(filename) = entry.file_name().to_str() {
+        return Ok(FileInfo::new(
+            perm,
+            nlinks,
+            owner,
+            group,
+            size,
+            modified_time,
+            filename.to_string(),
+            filetype,
+        ));
     }
 
-    #[allow(non_snake_case)]
-    fn with_F(&mut self) {
-        self.F = true;
-    }
-
-    fn with_h(&mut self) {
-        self.h = true;
-    }
-
-    #[allow(non_snake_case)]
-    fn with_S(&mut self) {
-        self.S = true;
-    }
-
-    #[allow(non_snake_case)]
-    fn with_R(&mut self) {
-        self.R = true;
-    }
-
-    fn with_r(&mut self) {
-        self.r = true;
-    }
-
-    fn with_t(&mut self) {
-        self.t = true;
-    }
-
-    fn with_d(&mut self) {
-        self.d = true;
-    }
+    Err("Error occured".into())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn file_mode_777() {
+        let mode: u32 = 40777;
+        let mode = get_mode(&FileType::File, mode);
+        assert_eq!("-rwxrwxrwx".to_string(), mode)
+    }
+
+    #[test]
+    fn file_mode_465() {
+        let mode: u32 = 40465;
+        let mode = get_mode(&FileType::File, mode);
+        println!("hellp? {mode}");
+        assert_eq!("-r--rw-r-x0".to_string(), mode)
+    }
+
+    #[test]
+    fn file_mode_000() {
+        let mode: u32 = 40000;
+        let mode = get_mode(&FileType::File, mode);
+        assert_eq!("----------".to_string(), mode)
+    }
 
     #[test]
     fn no_arguments() {
@@ -156,7 +356,7 @@ mod tests {
         let args = to_string_vec(vec!["mini-ls", "-l"]);
         let config = crate::Config::build(args.into_iter());
         let mut expected = Config::new();
-        expected.with_l();
+        expected.set_flag('l');
         assert_eq!(config, Ok(expected));
     }
 
@@ -165,8 +365,8 @@ mod tests {
         let args = to_string_vec(vec!["mini-ls", "-la"]);
         let config = crate::Config::build(args.into_iter());
         let mut expected = Config::new();
-        expected.with_l();
-        expected.with_a();
+        expected.set_flag('l');
+        expected.set_flag('a');
         assert_eq!(config, Ok(expected));
     }
 
@@ -175,8 +375,8 @@ mod tests {
         let args = to_string_vec(vec!["mini-ls", "-a", "-l"]);
         let config = crate::Config::build(args.into_iter());
         let mut expected = Config::new();
-        expected.with_l();
-        expected.with_a();
+        expected.set_flag('l');
+        expected.set_flag('a');
         assert_eq!(config, Ok(expected));
     }
 
@@ -185,8 +385,8 @@ mod tests {
         let args = to_string_vec(vec!["mini-ls", "-al"]);
         let config = crate::Config::build(args.into_iter());
         let mut expected = Config::new();
-        expected.with_l();
-        expected.with_a();
+        expected.set_flag('l');
+        expected.set_flag('a');
         assert_eq!(config, Ok(expected));
     }
 
@@ -196,8 +396,8 @@ mod tests {
         let config = crate::Config::build(args.into_iter());
         let mut expected = Config::new();
         expected.set_path(String::from("projects/"));
-        expected.with_l();
-        expected.with_a();
+        expected.set_flag('l');
+        expected.set_flag('a');
         assert_eq!(config, Ok(expected));
     }
 
@@ -207,8 +407,8 @@ mod tests {
         let config = crate::Config::build(args.into_iter());
         let mut expected = Config::new();
         expected.set_path(String::from("projects/"));
-        expected.with_l();
-        expected.with_a();
+        expected.set_flag('l');
+        expected.set_flag('a');
         assert_eq!(config, Ok(expected));
     }
 
